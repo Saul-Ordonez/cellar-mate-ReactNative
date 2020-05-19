f25d699a333a35413c9ab67ca952a80f7eb43b19
Saul-Ordonez <sauleordonez@gmail.com>
2020-05-19 16:01:11 -0700

    add field where logo will be placed on homescreen

--- src/api/jsonServer.js
+++ src/api/jsonServer.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://79d82283.ngrok.io'
+  baseURL: 'http://aebb83ef.ngrok.io'
 });
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -4,20 +4,47 @@ import { View, Text, StyleSheet, Button } from 'react-native';
 const HomeScreen = ({ navigation }) => {
   return (
     <View>
-      <Button 
-        title='View your cellar'
-        onPress={() => navigation.navigate('Index')}
-      />
-      <Button 
-        title='Add a beer'
-        onPress={() => navigation.navigate('Create')}
-      />
+      <Text style={styles.image} >Image Goes Here</Text>
+      <View style={styles.buttonGroup} >
+        <Button
+          style={styles.cellarButton}
+          title='View your cellar'
+          onPress={() => navigation.navigate('Index')}
+        />
+        <Button 
+          style={styles.addBeerButton}
+          title='Add a beer'
+          onPress={() => navigation.navigate('Create')}
+        />
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  
+  buttonGroup: {
+    marginTop: 75,
+  },
+  cellarButton: {
+    marginTop: 25,
+    borderColor: 'red',
+    borderWidth: 2,
+  },
+  addBeerButton: {
+
+  },
+  image: {
+    borderWidth: 2,
+    borderColor: 'red',
+    marginLeft: 20,
+    marginRight: 20,
+    marginTop: 50,
+    paddingTop: 125,
+    paddingBottom: 125,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 18
+  }
 });
 
 export default HomeScreen;
